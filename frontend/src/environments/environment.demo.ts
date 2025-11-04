f00005e0e3d115e4523d7abf8e818bb21fb8c17c
pedro <sakki.backup@gmail.com>
2025-11-04 19:57:18 -0300

    focus now atualização

--- frontend/src/environments/environment.demo.ts
+++ frontend/src/environments/environment.demo.ts
@@ -3,7 +3,7 @@ export const environment = {
   showCelebrationTester: false,
   // Set this to your laptop IP for Android device testing over Wi‑Fi
   // Example: 'http://192.168.0.42:3000'
-  apiBaseUrl: 'http://YOUR_LAN_IP:3000',
+  apiBaseUrl: 'http://10.87.20.166:3000',
   // For HTTP-only LAN demo, use token header instead of cookies
   useCookies: false
 };
